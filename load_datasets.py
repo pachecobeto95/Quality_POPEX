8c7274dd9c724c5d9491a0e9c2f199a4c7f0fbf1
Roberto Pacheco <robertopvc@gmail.com>
2021-02-02 17:03:40 -0300

    load datasets

---
+++ load_datasets.py
@@ -0,0 +1,66 @@
+import torchvision.transforms as transforms
+from torchvision import datasets
+
+class LoadDataset():
+  def __init__(self, input_dim, batch_size):
+    self.input_dim = input_dim
+    self.batch_size = batch_size
+
+    self.transformation_list = [transforms.Resize(256),
+                           transforms.CenterCrop(self.input_dim),
+                           transforms.ToTensor(),
+                           transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))]
+
+
+  def cifar_10(self):
+    # Load Cifar-10 dataset 
+    root = "cifar_10"
+
+    trainset = datasets.CIFAR10(root=root, train=True, download=True,
+                                transform=transforms.Compose(self.transformation_list))
+    
+    trainLoader = torch.utils.data.DataLoader(trainset, batch_size=self.batch_size, 
+                                              num_workers=2, shuffle=True, drop_last=True)
+    
+    testset = datasets.CIFAR10(root=root, train=False, download=True,
+                               transform=transforms.Compose(transformation_list))
+    
+    testLoader = torch.utils.data.DataLoader(testset, batch_size=self.batch_size, num_workers=2, shuffle=False)
+    
+    return trainLoader, testLoader
+
+  def cifar_100(self):
+    # Load Cifar-100 dataset
+    root = "cifar_100"
+
+    trainset = datasets.CIFAR100(root=root, train=True, download=True,
+                                transform=transforms.Compose(self.transformation_list))
+    
+    trainLoader = torch.utils.data.DataLoader(trainset, batch_size=self.batch_size, 
+                                              num_workers=2, shuffle=True, drop_last=True)
+    
+    testset = datasets.CIFAR100(root=root, train=False, download=True,
+                               transform=transforms.Compose(self.transformation_list))
+    
+    testLoader = torch.utils.data.DataLoader(testset, batch_size=self.batch_size, num_workers=2, shuffle=False)
+    
+    return trainLoader, testLoader
+
+  def imageNet(self):
+    # Load ImageNet Dataset
+    root = "ImageNet"
+
+    trainset = datasets.CIFAR100(root=root, train=True, download=True,
+                                transform=transforms.Compose(self.transformation_list))
+    
+    trainLoader = torch.utils.data.DataLoader(trainset, batch_size=self.batch_size, 
+                                              num_workers=2, shuffle=True, drop_last=True)
+    
+    testset = datasets.CIFAR100(root=root, train=False, download=True,
+                               transform=transforms.Compose(self.transformation_list))
+    
+    testLoader = torch.utils.data.DataLoader(testset, batch_size=self.batch_size, num_workers=2, shuffle=False)
+    
+    return trainLoader, testLoader
+
+
